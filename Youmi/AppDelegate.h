9a4b5ff5895f382514c98b6edb60910b8c08e868
noprom <tyee.noprom@qq.com>
2015-07-21 16:49:32 +0800

    Initial Commit

---
+++ Youmi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Youmi
+//
+//  Created by noprom on 15/7/21.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Youmi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Youmi
+//
+//  Created by noprom on 15/7/21.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
